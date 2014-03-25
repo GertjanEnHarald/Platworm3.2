91083db3a2b4193f3c65fa365a8e4b2d70b2f2a2
GertjanEnHarald <spamport@hotmail.com>
2014-03-25 18:10:15 +0100

    Vreemd
    Math.cos(Pi/2) geeft geen error

--- tests/worms/model/Tester.java
+++ tests/worms/model/Tester.java
@@ -6,7 +6,7 @@ public class Tester {
 		// TODO Auto-generated method stub
 		Worm testWorm = new Worm(-5,0,Math.PI/2,1,"James O'Hare");
 		System.out.println(testWorm.getJumpTime());
-		System.out.println(Math.pow(10, -10));
+		System.out.println(10/Math.cos(Math.PI/2));
 	}
 
 }
