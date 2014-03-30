c5c9284599c3060eba114684a91896195f42e111
GertjanEnHarald <spamport@hotmail.com>
2014-03-30 16:06:24 +0200

    Tester

--- tests/worms/model/Tester.java
+++ tests/worms/model/Tester.java
@@ -3,10 +3,10 @@ package worms.model;
 public class Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Worm testWorm = new Worm(-5,0,Math.PI/2,1,"James O'Hare");
-		System.out.println(testWorm.getJumpTime());
-		System.out.println(10/Math.cos(Math.PI/2));
+		for (int number = 1; number <= 12; number++) {
+		    System.out.println(number + " squared is " + (number * number));
+		}
+			
+	
 	}
-
 }
