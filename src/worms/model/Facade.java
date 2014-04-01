db7aaaeb631fc85cf3033bbdcf7d2f47b86d338d
GertjanEnHarald <spamport@hotmail.com>
2014-04-01 11:54:34 +0200

    Facade

--- src/worms/model/Facade.java
+++ src/worms/model/Facade.java
@@ -1,155 +1,336 @@
 package worms.model;
 
+import java.util.Collection;
+import java.util.Random;
+
 public class Facade implements IFacade {
-	
-	
-	public Worm createWorm(double x, double y, double direction, double radius, String name){
-		return new Worm(x, y,direction,radius,name);
-	}
-	
-	public boolean canMove(Worm worm, int nbSteps){
-		try {
-			return worm.canMove(nbSteps);
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
-	
-	public void move(Worm worm, int nbSteps){
-		try {
-			worm.move(nbSteps);
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
-	
-	public boolean canTurn(Worm worm, double angle){
-		try {
-			return worm.canTurn(angle);
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
+
+	@Override
+	public void addEmptyTeam(World world, String newName) {
+		// TODO Auto-generated method stub
 		
-	public void turn(Worm worm, double angle){
-		try {
-			worm.turn(angle);
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
+	}
+
+	@Override
+	public void addNewFood(World world) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addNewWorm(World world) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean canFall(Worm worm) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canMove(Worm worm) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canTurn(Worm worm, double angle) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Food createFood(World world, double x, double y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public World createWorld(double width, double height,
+			boolean[][] passableMap, Random random) {
+		
+		 return new World(width,height,passableMap,random);
+	}
+
+	@Override
+	public Worm createWorm(World world, double x, double y, double direction,
+			double radius, String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void fall(Worm worm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getActionPoints(Worm worm) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Projectile getActiveProjectile(World world) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Worm getCurrentWorm(World world) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Food> getFood(World world) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getHitPoints(Worm worm) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double[] getJumpStep(Projectile projectile, double t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double[] getJumpStep(Worm worm, double t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getJumpTime(Projectile projectile, double timeStep) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getJumpTime(Worm worm, double timeStep) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMass(Worm worm) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMaxActionPoints(Worm worm) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMaxHitPoints(Worm worm) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMinimalRadius(Worm worm) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getName(Worm worm) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getOrientation(Worm worm) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getRadius(Food food) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getRadius(Projectile projectile) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getRadius(Worm worm) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getSelectedWeapon(Worm worm) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTeamName(Worm worm) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getWinner(World world) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Worm> getWorms(World world) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getX(Food food) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getX(Projectile projectile) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getX(Worm worm) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getY(Food food) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getY(Projectile projectile) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getY(Worm worm) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isActive(Food food) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isActive(Projectile projectile) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAdjacent(World world, double x, double y, double radius) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAlive(Worm worm) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isGameFinished(World world) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isImpassable(World world, double x, double y, double radius) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void jump(Projectile projectile, double timeStep) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void jump(Worm worm, double timeStep) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move(Worm worm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rename(Worm worm, String newName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selectNextWeapon(Worm worm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setRadius(Worm worm, double newRadius) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void shoot(Worm worm, int yield) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startGame(World world) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startNextTurn(World world) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turn(Worm worm, double angle) {
+		// TODO Auto-generated method stub
+		
 	}
 	
-	public void jump(Worm worm){
-		try {
-			worm.jump();
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
-	
-	public double getJumpTime(Worm worm){
-		try {
-			return worm.getJumpTime();
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
-	
-	public double[] getJumpStep(Worm worm, double t){
-		try{
-			return worm.getJumpStep(t);
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
-	
-	public double getX(Worm worm){
-		try {
-			return worm.getCoordinateX();
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
-	
-	public double getY(Worm worm){
-		try {
-			return worm.getCoordinateY();
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
-	
-	public double getOrientation(Worm worm){
-		try {
-			return worm.getDirection();
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
-	
-	public double getRadius(Worm worm){
-		try {
-			return worm.getRadius();
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
-	
-	public void setRadius(Worm worm, double newRadius){
-		try {
-			worm.changeRadius(newRadius);
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
-	
-	public double getMinimalRadius(Worm worm){
-		try {	
-			return Worm.getMinimumRadius();
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
-	
-	public int getActionPoints(Worm worm){
-		try {
-			return worm.getActionPoints();
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
-	
-	public int getMaxActionPoints(Worm worm){
-		try {
-			return worm.getMaximumActionPoints();
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
-	
-	
-	public String getName(Worm worm){
-		try {
-			return worm.getName();
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
-	
-	public void rename(Worm worm, String newName){
-		try {
-			worm.setName(newName);
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
-	
-	public double getMass(Worm worm){
-		try {
-			return worm.getMass();
-		} catch (NullPointerException exc) {
-			throw new ModelException("Null is invalid argument!");
-		}
-	}
+
 }	
 
