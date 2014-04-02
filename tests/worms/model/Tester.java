3dad6dff88911a98a831391495a34245f923730e
GMT104 <gertjan_maenhout@hotmail.com>
2014-04-02 11:55:36 +0200

    tester

--- tests/worms/model/Tester.java
+++ tests/worms/model/Tester.java
@@ -3,10 +3,11 @@ package worms.model;
 public class Tester {
 
 	public static void main(String[] args) {
-		for (int number = 1; number <= 12; number++) {
-		    System.out.println(number + " squared is " + (number * number));
-		}
-			
+		//for (int number = 1; number <= 12; number++) {
+		//    System.out.println(number + " squared is " + (number * number));
+		//}
+		double x= 1.9999;
+		System.out.println((int)x);
 	
 	}
 }
